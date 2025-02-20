cbb9e93ec9e3afd6afbc6a7b9164c8e96710ada2
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2025-02-20 10:24:41 -0500

    Creación del proyecto.

---
+++ springboot-crud-api-restfull/src/main/java/com/sergio/curso/sprinboot/app/springboot_crud_api_restfull/SpringbootCrudApiRestfullApplication.java
@@ -0,0 +1,13 @@
+package com.sergio.curso.sprinboot.app.springboot_crud_api_restfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudApiRestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudApiRestfullApplication.class, args);
+	}
+
+}
---
+++ springboot-crud-api-restfull/src/test/java/com/sergio/curso/sprinboot/app/springboot_crud_api_restfull/SpringbootCrudApiRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergio.curso.sprinboot.app.springboot_crud_api_restfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudApiRestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
