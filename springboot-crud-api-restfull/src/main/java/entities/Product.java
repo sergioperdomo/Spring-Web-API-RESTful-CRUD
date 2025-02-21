28ddb63150246b8aa63e181dacf0bda06b91286c
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2025-02-21 15:53:01 -0500

    Creación de la clase Product

---
+++ springboot-crud-api-restfull/src/main/java/entities/Product.java
@@ -0,0 +1,23 @@
+package entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer price;
+    private String description;
+}
