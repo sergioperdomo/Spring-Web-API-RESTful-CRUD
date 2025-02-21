2e9976dc975fedd45606253d41132651b5663a6f
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2025-02-21 15:53:20 -0500

    Creación del repositorio para la clase Product

---
+++ springboot-crud-api-restfull/src/main/java/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+}
