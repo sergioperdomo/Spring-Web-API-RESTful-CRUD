ded7d70ba0f6f9cd034fb596c0fe5938228abd4e
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2025-02-22 17:49:57 -0500

    Añadiendo el decorador @Data

--- springboot-crud-api-restfull/src/main/java/com/sergio/curso/sprinboot/app/springboot_crud_api_restfull/entities/Product.java
+++ springboot-crud-api-restfull/src/main/java/com/sergio/curso/sprinboot/app/springboot_crud_api_restfull/entities/Product.java
@@ -1,17 +1,16 @@
-package entities;
+package com.sergio.curso.sprinboot.app.springboot_crud_api_restfull.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+
 
 @Entity
 @Table(name = "products")
-@Getter
-@Setter
+@Data
 public class Product {
 
     @Id
