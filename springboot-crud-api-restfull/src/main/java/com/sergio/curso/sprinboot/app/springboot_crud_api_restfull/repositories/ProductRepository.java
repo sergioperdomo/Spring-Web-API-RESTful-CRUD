cf08f9b4ce563dacd710f910f4a1e96a1d8e75ad
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2025-02-22 17:50:28 -0500

    Añadiendo el decorador @Repository

---
+++ springboot-crud-api-restfull/src/main/java/com/sergio/curso/sprinboot/app/springboot_crud_api_restfull/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.sergio.curso.sprinboot.app.springboot_crud_api_restfull.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sergio.curso.sprinboot.app.springboot_crud_api_restfull.entities.Product;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
+}
