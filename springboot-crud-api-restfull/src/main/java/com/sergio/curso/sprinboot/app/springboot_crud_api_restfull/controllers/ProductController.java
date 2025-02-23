d2cfc47fe3833155b9918733427b60cd7a229f52
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2025-02-23 09:30:58 -0500

    Agragando la anotación @Valid para hacer validaciones.
    En el caso de que el JSON no cumpla con las restricciones, Spring devuelve un error de validación sin necesidad de código extra.

--- springboot-crud-api-restfull/src/main/java/com/sergio/curso/sprinboot/app/springboot_crud_api_restfull/controllers/ProductController.java
+++ springboot-crud-api-restfull/src/main/java/com/sergio/curso/sprinboot/app/springboot_crud_api_restfull/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.sergio.curso.sprinboot.app.springboot_crud_api_restfull.controllers;
 import com.sergio.curso.sprinboot.app.springboot_crud_api_restfull.entities.Product;
 import com.sergio.curso.sprinboot.app.springboot_crud_api_restfull.services.ProductService;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -33,12 +34,12 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> create(@RequestBody Product product){
+    public ResponseEntity<Product> create(@Valid @RequestBody Product product){
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(product));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product fieldUpdateProduct){
+    public ResponseEntity<Product> update(@PathVariable Long id, @Valid @RequestBody Product fieldUpdateProduct){
         try {
             Product updatedProduct = productService.update(id, fieldUpdateProduct);
             return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
