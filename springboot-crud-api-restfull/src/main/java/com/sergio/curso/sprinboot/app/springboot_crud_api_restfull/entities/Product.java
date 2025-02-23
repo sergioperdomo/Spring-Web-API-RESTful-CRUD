3b89b5f4e27d70870ce872e518482347a17d8628
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2025-02-23 09:28:37 -0500

    Agregando las anotaciones: - @NotBlank: Indica que el campo no puede ser null, ni una cadena vacía (""), ni contener solo espacios en blanco (" "). - @NotNull: El campo no debe ser nulo. - @Positive: Asegura que el precio sea mayor a 0 (cero), y no permite valores negativos ni 0 (cero)
    En los respectivos atributos de la entidad Product

--- springboot-crud-api-restfull/src/main/java/com/sergio/curso/sprinboot/app/springboot_crud_api_restfull/entities/Product.java
+++ springboot-crud-api-restfull/src/main/java/com/sergio/curso/sprinboot/app/springboot_crud_api_restfull/entities/Product.java
@@ -5,6 +5,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 
@@ -13,10 +17,23 @@ import lombok.Data;
 @Data
 public class Product {
 
+    /***
+     * Para hacer validaciónes se usa el @Valid antes del @RequestBody como se muestra a continuación:
+     * @see  com.sergio.curso.sprinboot.app.springboot_crud_api_restfull.controllers.ProductController#update(Long, Product)
+     */
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 20)
     private String name;
+
+    @NotNull
+    @Positive(message = "El precio debe ser mayor a cero")
     private Integer price;
+
+    @NotBlank
     private String description;
 }
