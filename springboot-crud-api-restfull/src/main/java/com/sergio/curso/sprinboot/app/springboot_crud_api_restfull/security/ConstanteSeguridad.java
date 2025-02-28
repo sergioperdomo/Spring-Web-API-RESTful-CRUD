f504819eb1342dcfe3ebcf3a28417d21d03b8b8f
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2025-02-28 17:08:30 -0500

    Define constantes de seguridad para la gestión y firma de tokens JWT.

--- springboot-crud-api-restfull/src/main/java/com/sergio/curso/sprinboot/app/springboot_crud_api_restfull/security/ConstanteSeguridad.java
+++ springboot-crud-api-restfull/src/main/java/com/sergio/curso/sprinboot/app/springboot_crud_api_restfull/security/ConstanteSeguridad.java
@@ -2,26 +2,12 @@ package com.sergio.curso.sprinboot.app.springboot_crud_api_restfull.security;
 
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
 
-
-@Component
 public class ConstanteSeguridad {
 
-    public static long JWT_EXPIRATION_TOKEN; // 5 minutos.
-    public static Key JWT_FIRMA;
-
-    @Value("${jwt.expiration}")
-    public void setJwtExpirationToken(long expirationToken){
-        JWT_EXPIRATION_TOKEN = expirationToken;
-    }
-
-    @Value("${jwt.secret}")
-    public void setJwtFirma(String secretKey){
-        JWT_FIRMA = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secretKey));
-    }
-
+    public static final long  JWT_EXPIRATION_TOKEN = 300000; // 5 minutos.
+    public static final String JWT_SECRET = "abcdefghijklmnopqrstuvwxzy123456";
+    public static final SecretKey  JWT_FIRMA = Keys.hmacShaKeyFor(Decoders.BASE64.decode(JWT_SECRET));
 }
