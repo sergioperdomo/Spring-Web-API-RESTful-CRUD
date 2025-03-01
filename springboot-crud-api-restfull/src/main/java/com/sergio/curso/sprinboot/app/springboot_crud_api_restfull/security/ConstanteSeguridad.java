c28438f003998cee92e8b484c250b8e2429fdaea
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2025-03-01 13:35:07 -0500

    Cambiando la clave secreta.

--- springboot-crud-api-restfull/src/main/java/com/sergio/curso/sprinboot/app/springboot_crud_api_restfull/security/ConstanteSeguridad.java
+++ springboot-crud-api-restfull/src/main/java/com/sergio/curso/sprinboot/app/springboot_crud_api_restfull/security/ConstanteSeguridad.java
@@ -8,6 +8,6 @@ import javax.crypto.SecretKey;
 public class ConstanteSeguridad {
 
     public static final long  JWT_EXPIRATION_TOKEN = 300000; // 5 minutos.
-    public static final String JWT_SECRET = "abcdefghijklmnopqrstuvwxzy123456";
+    public static final String JWT_SECRET = "A0/zdhyORZNijz0D0tZ9DEy9i87kfBsXGxvRbTacZZE=";
     public static final SecretKey  JWT_FIRMA = Keys.hmacShaKeyFor(Decoders.BASE64.decode(JWT_SECRET));
 }
