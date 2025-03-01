6681b4bf43edff607d9b14404c04bf4fa17483e3
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2025-03-01 13:36:47 -0500

    Creación de métodos y siu implementación.

--- springboot-crud-api-restfull/src/main/java/com/sergio/curso/sprinboot/app/springboot_crud_api_restfull/services/UserService.java
+++ springboot-crud-api-restfull/src/main/java/com/sergio/curso/sprinboot/app/springboot_crud_api_restfull/services/UserService.java
@@ -1,11 +1,16 @@
 package com.sergio.curso.sprinboot.app.springboot_crud_api_restfull.services;
 
-import com.sergio.curso.sprinboot.app.springboot_crud_api_restfull.entities.User;
+import com.sergio.curso.sprinboot.app.springboot_crud_api_restfull.dtos.DtoAuthResponse;
+import com.sergio.curso.sprinboot.app.springboot_crud_api_restfull.dtos.UserDto;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
 
-    List<User> findAll();
-    User save(User user);
+    List<UserDto> findAll();
+    UserDto save(UserDto userDto);
+    Optional<UserDto> getUserByUsername(String username);
+    boolean existsByUsername(String username);
+    Optional<DtoAuthResponse> login(UserDto userDtoLogin);
 }
