9c6d35e5ec2e155022898ac37777c905ee2169ff
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2025-03-04 11:20:01 -0500

    Eliminando campos que no se estan usando.

--- springboot-crud-api-restfull/src/main/java/com/sergio/curso/sprinboot/app/springboot_crud_api_restfull/security/SecurityConfig.java
+++ springboot-crud-api-restfull/src/main/java/com/sergio/curso/sprinboot/app/springboot_crud_api_restfull/security/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.sergio.curso.sprinboot.app.springboot_crud_api_restfull.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,7 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final CustomUsersDetailsService customUsersDetailsService;
     private final JwtGenerador jwtGenerador;
 
